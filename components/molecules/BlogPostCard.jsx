6e842408a4a110f2253b79a1f972bbde95482516
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-01-24 12:48:48 +0100

    fixed picture issue

--- components/molecules/BlogPostCard.jsx
+++ components/molecules/BlogPostCard.jsx
@@ -18,7 +18,7 @@ export default function BlogPostCard({
     <Link href={`/blog/${id}`}>
       <article className='flex flex-col w-full mb-3 bg-white bg-opacity-50 rounded-lg shadow cursor-pointer md:flex-row hover:shadow-md hover:bg-opacity-100'>
         <img
-          className='object-cover rounded-t-lg max-h-52 md:rounded-l-lg md:rounded-t-none md:w-64 '
+          className='object-cover rounded-t-lg max-h-52 md:rounded-l-lg md:rounded-t-none md:w-64 md:max-h-full'
           src={hasThumbnail ? image : '/images/placeholder.svg'}
           alt=''
           loading='lazy'
