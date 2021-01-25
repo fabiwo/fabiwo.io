e0043152ef540194668e3177b18703bc4ceb240e
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-01-25 07:56:39 +0100

    added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,32 @@ class MyDocument extends Document {
             type='font/woff2'
             crossOrigin='anonymous'
           />
+          <link rel='shortcut icon' href='/static/favicons/favicon.ico' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/static/favicons/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/static/favicons/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/static/favicons/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/static/favicons/site.webmanifest' />
+          <link
+            rel='mask-icon'
+            href='/static/favicons/safari-pinned-tab.svg'
+            color='#5bbad5'
+          />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#df4f4f' />
         </Head>
         <body>
           <Main />
