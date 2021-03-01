a18c3b1f1edf9d7f270dcf947605a0518993ab8e
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-03-01 09:27:07 +0100

    try ganalytics key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
     BUTTONDOWN_API_KEY: process.env.BUTTONDOWN_API_KEY,
+    NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
