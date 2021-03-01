5e53dbca36e155d4fa4e0c7a287271c762f44e0a
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-03-01 09:08:14 +0100

    corrected gtag not exposed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   env: {
     BUTTONDOWN_API_KEY: process.env.BUTTONDOWN_API_KEY,
-    GA_ID: process.env.GA_ID,
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
