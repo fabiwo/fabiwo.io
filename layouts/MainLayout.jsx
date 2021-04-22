739101bb2666999425c5033d7968e828596eb67b
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-04-22 07:30:34 +0200

    added option to disable newsletter in MainLayout.jsx

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import NewsletterForm from '@/components/Newsletter/NewsletterForm'
 
-export default function MainLayout({ children }) {
+export default function MainLayout({ children, withNewsletter = true }) {
   return (
     <div className='relative flex flex-col items-start justify-center max-w-3xl mx-auto px-7'>
       {children}
-      <NewsletterForm />
+      {withNewsletter && <NewsletterForm />}
     </div>
   )
 }
